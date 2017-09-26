b4d018f9e65ad0357cfc7e93acd6925229998cf5
Tibor Leupold <tibor.leupold@gmail.com>
2017-09-26 21:43:23 +0200

    Just added some comments and prints for clarity.

--- fill-in-the-gaps.py
+++ fill-in-the-gaps.py
@@ -2,7 +2,13 @@
 
 print("### Fill in the Gaps ###")
 
-def play_game(problem_phrase, wrong_guesses_limit, list_of_tuples_placeholder_and_answer):
+def play_game(problem_phrase, list_of_tuples_placeholder_and_answer, wrong_guesses_limit):
+    """This is the main function to play the fill-in-the-gaps game. It takes the
+    problem phrase (the text with the gaps), a list of tuples and the maximum
+    number of wrong guesses as inputs. 
+    Each tuple in the list has to contain the placeholder (used in the problem 
+    phrase) and it's correct replacement.
+    This function always returns None."""
     wrong_guesses_counter = 0
     placeholder_index = 0
     max_placeholder_index =  len(list_of_tuples_placeholder_and_answer) - 1
@@ -26,7 +32,8 @@ def play_game(problem_phrase, wrong_guesses_limit, list_of_tuples_placeholder_an
             print("Sorry, but '{}' does not fit {}.".format(user_answer, placeholder))
             wrong_guesses_counter += 1
 
-    # The game is over. Trying to find out which way it ended.
+    # The game is over. 
+    # Time to tell the player how it ended.
     if wrong_guesses_counter > wrong_guesses_limit:
         print("You have reached the limit of wrong guesses!")
     elif placeholder_index > max_placeholder_index:
@@ -40,13 +47,21 @@ def play_game(problem_phrase, wrong_guesses_limit, list_of_tuples_placeholder_an
     return None
 
 
-print("")
+print("###################")
 print("Testing")
+print("###################")
+
+print("")
+print("Testing play_game() - 1")
 print("")
 play_game(
     problem_phrase="This is a __1__.", 
     wrong_guesses_limit=1, 
     list_of_tuples_placeholder_and_answer=[("__1__","placeholder")])
+
+
+print("")
+print("Testing play_game() - 2")
 print("")
 play_game(
     problem_phrase="This is a __1__. And that's another __2__.", 
