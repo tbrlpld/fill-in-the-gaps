8f72e0cda98df9ff2e3bd98eee5382a7c800bf3f
Tibor Leupold <tibor.leupold@gmail.com>
2017-10-02 22:16:17 +0200

    Reduced additional output/print of the test script.

--- testing/test_play_game.py
+++ testing/test_play_game.py
@@ -5,18 +5,18 @@ print("###################")
 print("Testing")
 print("###################")
 
-print("")
+# print("")
 print("Testing play_game() - 1")
-print("")
+# print("")
 play_game(
     problem_phrase="This is a __1__.", 
     wrong_guesses_limit=1, 
     list_of_tuples_placeholder_and_answer=[("__1__","placeholder")])
 
 
-print("")
+# print("")
 print("Testing play_game() - 2")
-print("")
+# print("")
 play_game(
     problem_phrase="This is a __1__. And that's another __2__.", 
     wrong_guesses_limit=1, 
