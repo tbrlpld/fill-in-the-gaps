823dd3c1199dd4e785b4cebaa6c5e5cfaf1e90de
Tibor Leupold <tibor.leupold@gmail.com>
2017-10-10 19:49:03 +0200

    Added testing for overwritable_slice to git.

---
+++ testing/test_overwritable_slice.py
@@ -0,0 +1,27 @@
+from fill_in_the_gaps import print_overwritable_slice
+
+
+test_string = "test"
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = len(test_string))
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 0)
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 1)
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 2)
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 3)
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 4)
+
+print("")
+print_overwritable_slice(string_to_print = test_string, slice_end = 10)
+
+print("")
